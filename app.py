72524098e6d7532087f1a26e61204466fbc1f72e
wjlee-ling <wjlee.ling@gmail.com>
2023-06-27 15:28:36 +0900

    [new] add image

--- app.py
+++ app.py
@@ -34,7 +34,11 @@ st.title("경기청년 갭이어 프로그램 FAQ")
 user_input = st.text_input(label='무엇이 궁금하세요?', key="user_input", on_change=get_response)
 
 if "ans" in sst:
-    st.success(sst.ans)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.success(sst.ans)
+    with col2:
+        st.image("https://github.com/boostcampaitech4lv23nlp2/final-project-level3-nlp-13/assets/61496071/3c7b10ff-5bc5-4006-8400-d234df523c46")
 
 if "json" in sst:
     with st.expander("More info"): 
