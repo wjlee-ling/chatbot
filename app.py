9aa88c9aa1a5b950bcaa6d7fd18cdacf53638e20
wjlee-ling <wjlee.ling@gmail.com>
2023-06-27 15:55:44 +0900

    [add] suggestions

--- app.py
+++ app.py
@@ -24,21 +24,32 @@ def parse(text):
     pattern_url = re.compile(r'(?<={"url":").+?(?="})')
     match_reply = re.search(pattern_reply, text)
     if match_reply:
-        sst.ans = match_reply.group(0)
+        sst.ans = match_reply.group(0).replace(r"\n", "\n")
         if match_url:= re.search(pattern_url, text):
             sst.ans += f"\n{match_url.group(0)}"
     else:
         sst.ans = "다시 submit 버튼을 눌러주세요"
 
 st.title("경기청년 갭이어 프로그램 FAQ")
-user_input = st.text_input(label='무엇이 궁금하세요?', key="user_input", on_change=get_response)
+
+user_input = st.text_input(label="챗봇에게 물어보기👇", key="user_input", on_change=get_response)
+
+with st.expander("이런 건 안 궁금하세요?", expanded=True):
+    st.markdown("""
+    * 이 챗봇은 뭔가요?
+    * 경기청년 갭이어 프로그램이 뭔가요?
+    * 직장인도 갭이어 프로그램에 지원 가능한가요?
+    * 저도 지원할 수 있나요?
+    * 갭이어 선정 과정은요?
+    * 면접 질문은?
+    """)
 
 if "ans" in sst:
     col1, col2 = st.columns(2)
     with col1:
         st.success(sst.ans)
     with col2:
-        st.image("https://github.com/boostcampaitech4lv23nlp2/final-project-level3-nlp-13/assets/61496071/3c7b10ff-5bc5-4006-8400-d234df523c46")
+        st.image("https://github.com/boostcampaitech4lv23nlp2/final-project-level3-nlp-13/assets/61496071/3c7b10ff-5bc5-4006-8400-d234df523c46", caption="저는 시아예요")
 
 if "json" in sst:
     with st.expander("More info"): 
